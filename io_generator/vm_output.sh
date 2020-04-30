40649e8988a71e8aaf0d7fcb9fdc26a7af7c0b1d
Watson <a0916228544@gmail.com>
2020-04-30 22:54:35 +0800

    add organized data

--- io_generator/vm_output.sh
+++ io_generator/vm_output.sh
@@ -2,9 +2,9 @@
 gcc ../scheduler.c -DFRMGNR -o scheduler
 for p in FIFO SJF RR PSJF ;do
     for i in {1..5} ;do
-        echo "----------------------"$p"_$i----------------------------"
+        echo "----------------------"$p"_"$i"----------------------------"
         dmesg --clear
-        ./scheduler < "../input/"$p"_$i.txt" 1> "../output/"$p"_"$i"_stdout.txt"
+        ./scheduler < "../input/"$p"_"$i".txt" 1> "../output/"$p"_"$i"_stdout.txt"
         dmesg | grep Project1 > "../output/"$p"_"$i"_dmesg.txt"
     done
 done
